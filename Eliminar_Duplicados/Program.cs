b167396f21a935f32d9183c69a6edcc96e06ddf9
GianCarlo Guillen Salome <gianguillen77@gmail.com>
2026-01-01 23:40:43 -0500

    crea un nuevo array con los números ya no repetidos

--- Eliminar_Duplicados/Program.cs
+++ Eliminar_Duplicados/Program.cs
@@ -12,25 +12,35 @@ namespace Eliminar_Duplicados
     {
         static void Main(string[] args)
         {
-            int [] numeros = { 1, 23, 20, 40, 9, 23, 20, 1 };
+            int[] numeros = { 1, 23, 20, 40, 9, 23, 20, 1 };
+            int[] sinDuplicados = new int[numeros.Length];
+            int count = 0;
 
-            for (int i = 0; i < numeros.Length; i++)
+            for (int i = 0; i < numeros.Length; i++) 
             {
-                for (int j = i + 1; j < numeros.Length; j++)
+                bool repetido = false;
+
+                for (int j = 0; j < count; j++)
                 {
-                    if (numeros[i] == numeros[j])
+                    if (numeros[i] == sinDuplicados[j])
                     {
-                        numeros[j] = -1; // Marcar como duplicado
+                        repetido = true;
+                        break;
                     }
                 }
-            }
-            foreach (var numero in numeros)
-            {
-                if (numero != -1)
+
+                if (!repetido)
                 {
-                    Console.WriteLine(numero);
+                    sinDuplicados[count] = numeros[i];
+                    count++;
                 }
             }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(sinDuplicados[i]);
+            }
+
         }
     }
 }
